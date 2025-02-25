917b0fbbadc0d0874f3796a6031c7c80d510305c
Marty <mvelitchkov@icloud.com>
2025-02-25 17:36:31 +0100

    added sensing to ghosts

--- src/agents/Ghost.py
+++ src/agents/Ghost.py
@@ -4,8 +4,29 @@ import random
 class Ghost(BaseAgent.BaseAgent):
     color = "red"
     
+    def sense_pacman(self, pacman_pos):
+        ghost_x = self.position.x
+        ghost_y = self.position.y
+        pacman_x = pacman_pos.x
+        pacman_y = pacman_pos.y
+
+        if abs(ghost_x - pacman_x) < 10 and abs(ghost_y - pacman_y) < 3*20:
+            if ghost_y < pacman_y and self.valid(self.position + self.UP):
+                return self.UP
+            elif self.valid(self.position + self.DOWN):
+                return self.DOWN
+        elif abs(ghost_y - pacman_y) < 10 and abs(ghost_x - pacman_x) < 3*20:
+            if ghost_x < pacman_x and self.valid(self.position + self.RIGHT):
+                return self.RIGHT
+            elif self.valid(self.position + self.LEFT):
+                return self.LEFT
+        return None
+
     def step(self, game_state):
-        if self.course and self.valid(self.position + self.course):
+        if self.sense_pacman(game_state["pacman"]):
+            self.course = self.sense_pacman(game_state["pacman"])
+            self._move(self.course)
+        elif self.course and self.valid(self.position + self.course):
             self._move(self.course)
         else:
             options = [
